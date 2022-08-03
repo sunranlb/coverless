47b2be46357d467b74f330b09b57b8ad3afde504
SunRan <sr@Handel.local>
2022-08-03 11:29:51 +0800

    popwindow

--- content-script.js
+++ content-script.js
@@ -1,1 +1,14 @@
-console.log('aaaa')
+(function() {
+    function $$(selector, context) {
+        context = context || document;
+        var elements = context.querySelectorAll(selector);
+        return elements.length == 1
+            ? Array.prototype.slice.call(elements)[0]
+            : Array.prototype.slice.call(elements);
+    }
+
+    let cover = $$(".hide-article-box");
+    cover.remove();
+    let content = $$("#article_content");
+    content.style.height = "";
+})();
