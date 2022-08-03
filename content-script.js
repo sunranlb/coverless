32e64c7b33585d15fa9ca61c7a29e4b4374134ea
sr <sunranlb@qq.com>
2022-08-03 19:59:53 +0800

    51

--- content-script.js
+++ content-script.js
@@ -7,16 +7,27 @@
             : Array.prototype.slice.call(elements);
     }
 
+    function r(id) {
+        let cover = $$(id);
+        if (cover.length == undefined) {
+            console.log('Cover removed! ' + id);
+            cover.remove();
+        }
+    }
+    
     let host = window.location.host;
     if (host.indexOf('.csdn.net') > 0) {
-        let cover = $$(".hide-article-box");
-        cover.remove();
+        r(".hide-article-box");
         let content = $$("#article_content");
-        content.style.height = "";
-    } else if (host.indexOf('.zhihu.com')) {
-        let cover = $$(".Modal-wrapper");
-        cover.remove();
+        if (content.length == undefined) {
+            content.style.height = "";
+        }
+        r(".passport-login-container");
+    } else if (host.indexOf('.zhihu.com') > 0) {
+        r(".Modal-wrapper");
         let html = $$('html');
         html.style.overflow = "";
+    } else if (host.indexOf('jb51.cc') > 0) {
+        r("#read-more-wrap");
     }
 })();
