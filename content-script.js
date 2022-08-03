eaf9d2b7952346666aca95c060355fa1062e697a
sr <sunranlb@qq.com>
2022-08-03 22:44:35 +0800

    baidu

--- content-script.js
+++ content-script.js
@@ -1,10 +1,34 @@
-(function() {
+(function () {
     function $$(selector, context) {
         context = context || document;
         var elements = context.querySelectorAll(selector);
         return elements.length == 1
             ? Array.prototype.slice.call(elements)[0]
             : Array.prototype.slice.call(elements);
+    }
+
+    function debounce(fn, wait) {
+        var timer = null;
+        return function () {
+            if (timer !== null) {
+                clearTimeout(timer);
+            }
+            timer = setTimeout(fn, wait);
+        }
+    }
+
+    /**
+     * 动态监控
+     */
+    function registerObserver() {
+        let observer = new MutationObserver(debounce(() => {
+            main();
+        }, 200));
+        let article = document.body;
+        let options = {
+            'childList': true
+        };
+        observer.observe(article, options);
     }
 
     function r(id) {
@@ -14,20 +38,27 @@
             cover.remove();
         }
     }
-    
-    let host = window.location.host;
-    if (host.indexOf('.csdn.net') > 0) {
-        r(".hide-article-box");
-        let content = $$("#article_content");
-        if (content.length == undefined) {
-            content.style.height = "";
+
+    function main() {
+        let host = window.location.host;
+        if (host.indexOf('.csdn.net') >= 0) {
+            r(".hide-article-box");
+            let content = $$("#article_content");
+            if (content.length == undefined) {
+                content.style.height = "";
+            }
+            r(".passport-login-container");
+        } else if (host.indexOf('.zhihu.com') >= 0) {
+            r(".Modal-wrapper");
+            let html = $$('html');
+            html.style.overflow = "";
+        } else if (host.indexOf('jb51.cc') >= 0) {
+            r("#read-more-wrap");
+        } else if (host.indexOf('tieba.baidu.com') >= 0) {
+            r("#tiebaCustomPassLogin")
         }
-        r(".passport-login-container");
-    } else if (host.indexOf('.zhihu.com') > 0) {
-        r(".Modal-wrapper");
-        let html = $$('html');
-        html.style.overflow = "";
-    } else if (host.indexOf('jb51.cc') > 0) {
-        r("#read-more-wrap");
     }
+
+    main();
+    registerObserver();
 })();
